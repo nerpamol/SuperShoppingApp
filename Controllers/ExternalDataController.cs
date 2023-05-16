b14e873cd30de05f4377994dcdeefca0229da507
Nerpagar <2176491@cognizant.com>
2023-05-17 00:25:17 +0530

    MVC code added

---
+++ Controllers/ExternalDataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingWithAPI.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingWithAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
